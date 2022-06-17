3b84c5f0d7446d853dee0774451ca6ed628bc25b
Nader TCG <naderc@tcgcape.co.za>
2022-06-17 14:07:51 +0200

    Finished with the github notes & added a clear button to the counter

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 let countNo = 0;
+let counter = document.getElementById("counter");
 
 function count() {
 
     countNo++;
 
-    document.getElementById("counter").innerHTML = countNo;
+    console.log(countNo);
+
+    counter.innerHTML = countNo;
     
     document.getElementById("counterBtn").innerHTML = "Clicked!";
 
 }
+
+function counterClear() {
+
+    countNo = 0;
+
+    console.log(countNo);
+
+    counter.innerHTML = countNo;
+    
+    document.getElementById("counterBtn").innerHTML = "Click!";
+
+}
+
